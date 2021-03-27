9f0a0844d83c064b8d82ebf10022c693466c3743
Hengborann-Moul <moulhengborann@gmail.com>
2021-03-27 21:03:53 +0700

    Convert String to others type

---
+++ practice/convert_sting.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter some Number: ")
+	numInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	if err != nil {
+		// TODO: Fallback concept
+		fmt.Println(err)
+	} else {
+		fmt.Println("You entered: ", aFloat)
+	}
+}
